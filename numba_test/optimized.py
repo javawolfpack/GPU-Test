a1b69671c831a337c0747a07d471a496e7c240d4
Bryan Dixon <javawolfpack@gmail.com>
2019-08-18 15:35:18 -0700

    Python Opt AutoLoop

--- numba_test/optimized.py
+++ numba_test/optimized.py
@@ -1,26 +1,26 @@
 import sys
 import numpy as np
-import time
-import numba
+from numba import jit, int32, float64
 
-
-@numba.jit(nopython=True, parallel=True)
-def fun(a,b,n):
-    out = np.empty_like(a)
+@jit(float64(float64, float64, int32))
+def function(a, b, n):
+    result=[]
     for i in range(n):
-        c[i]=a[i]+b[i]
-    return c
+        result+=[a[i]+b[i]]
+
+    # your loop or numerically intensive computations
+    return result
+
 
 if len(sys.argv) < 2:
     print("Requires 1 argument, the number of elements in the array")
     quit()
 
 n=int(sys.argv[1])
-a=np.random.uniform(low=-100, high=100, size=(n), dtype=np.float32)
-b=np.random.uniform(low=-100, high=100, size=(n), dtype=np.float32)
-print(a.dtype)
+a=np.random.uniform(low=-100, high=100, size=(n))
+b=np.random.uniform(low=-100, high=100, size=(n))
 start = time.perf_counter()
-# returnc = fun(a,b,n)
+c=fun(a,b,n)
 end=time.perf_counter()
 # print(c)
 print("Elapsed Time: " + str(end - start))
