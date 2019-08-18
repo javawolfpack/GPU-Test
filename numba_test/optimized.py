6b9d1a7f6dd0220cb7fc623bfc83e5bf890b860a
Bryan Dixon <javawolfpack@gmail.com>
2019-08-18 16:29:08 -0700

    Python Opt AutoLoop

--- numba_test/optimized.py
+++ numba_test/optimized.py
@@ -1,15 +1,18 @@
 import sys
 import numpy as np
-from numba import njit, prange
+from numba import njit, prange, cuda
 import time
 
-@njit(parallel=True)
+# @njit(parallel=True)
+@cuda.jit
 def fun(a, b, n):
     result = np.zeros(n)
-    for i in prange(n):
-        # print(a[i])
-        result[i]=a[i]+b[i]
-
+    # for i in prange(n):
+    #     # print(a[i])
+    #     result[i]=a[i]+b[i]
+    pos = cuda.grid(1)
+    if pos < n:
+        result[pos]=a[pos] + b[pos]
     # your loop or numerically intensive computations
     return result
 
