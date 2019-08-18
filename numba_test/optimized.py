85c0825f9b090e83eea093748a40004c05c784ea
Bryan Dixon <javawolfpack@gmail.com>
2019-08-18 15:01:59 -0700

    Python Opt AutoLoop

---
+++ numba_test/optimized.py
@@ -0,0 +1,25 @@
+import sys
+import numpy as np
+import time
+import numba
+
+
+@numba.jit(nopython=True, parallel=True)
+def fun(a,b,n):
+    c=[]
+    for i in range(n):
+        c+=[a[i]+b[i]]
+    return c
+
+if len(sys.argv) < 2:
+    print("Requires 1 argument, the number of elements in the array")
+    quit()
+
+n=int(sys.argv[1])
+a=np.random.uniform(low=-100, high=100, size=(n))
+b=np.random.uniform(low=-100, high=100, size=(n))
+start = time.perf_counter()
+c=fun(a,b,n)
+end=time.perf_counter()
+# print(c)
+print("Elapsed Time: " + str(end - start))
