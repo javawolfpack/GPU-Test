f9e0c6d8a34d8daf6dfac3834652928a4b72848e
Bryan Dixon <javawolfpack@gmail.com>
2019-08-18 14:23:08 -0700

    Python Test added

--- numba_test/naive.py
+++ numba_test/naive.py
@@ -5,6 +5,9 @@ def fun(a,b,n):
         c+=[a[i]+b[i]]
     return c
 
+if len(sys.argv) < 2:
+    print("Requires 1 argument, the number of elements in the array")
+    quit()
 
 a=[1,2,3,4]
 b=[1,2,3,4]
