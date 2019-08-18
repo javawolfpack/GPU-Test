eba3f044bc410a04750df774f11535970684beab
Bryan Dixon <javawolfpack@gmail.com>
2019-08-18 14:25:08 -0700

    Python Test added

--- numba_test/naive.py
+++ numba_test/naive.py
@@ -10,7 +10,8 @@ if len(sys.argv) < 2:
     print("Requires 1 argument, the number of elements in the array")
     quit()
 
-a=[1,2,3,4]
-b=[1,2,3,4]
-c=fun(a,b,4)
+n=int(argv[1])
+a=[random.uniform(-100,100) for _ in xrange(n)]
+b=[random.uniform(-100,100) for _ in xrange(n)]
+c=fun(a,b,n)
 print(c)
